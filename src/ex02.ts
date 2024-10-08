9673977e81600fed2a1873d240e417d9f7cb258f
Saki Ebina <ebiebi0u0@gmail.com>
2024-10-08 16:01:11 -0700

    finished ex02

--- src/ex02.ts
+++ src/ex02.ts
@@ -3,9 +3,10 @@
 // Define an object of type `Data` where `value` is a string "Generic" and `id` is 1.
 // Log the object.
 
-interface Data {
-  
+interface Data<T,U> {
+  id:T
+  value:U
 }
 
-const data: Data = { id: 1, value: "Generic" };
+const data: Data<number,string> = { id: 1, value: "Generic" };
 console.log(data); // Expected output: { id: 1, value: "Generic" }
